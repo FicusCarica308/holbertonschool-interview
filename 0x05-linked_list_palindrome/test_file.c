50966a36595554c1270e73ecfeea9ad10dfacb2c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-08-27 00:12:32 +0000

    new and improved palindrome checking function that doesnt use recursion

--- 0x05-linked_list_palindrome/test_file.c
+++ 0x05-linked_list_palindrome/test_file.c
@@ -1,41 +1,37 @@
-#include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * is_palindrome - checks if a given linked list is a palindrome
- * @head: a double pointer to the head of a given linked list
- * Return: returns 0 if not palindrome and 1 if is a palindrome
- */
-int is_palindrome(listint_t **head)
-{
-	listint_t *temp_head1 = *head;
-	listint_t *temp_head2 = *head;
-	int *flipped_values = NULL;
-	int list_lgt = 0;
-
-	for (list_lgt = 0; temp_head1 != NULL; list_lgt++)
-		temp_head1 = temp_head1->next;
-
+if (list_lgt % 2 != 0)
+	{
+		is_odd++;
+	}
 	list_lgt = list_lgt / 2;
 
-	flipped_values = malloc(sizeof(int) * list_lgt);
+	flipped_values = (int *)malloc(sizeof(int) * list_lgt + 1);
 
 	while (temp_head2 != NULL)
 	{
-		if (list_lgt >= 0)
+		if (list_lgt > 0 && made_it == 0)
 		{
-			flipped_values[list_lgt] = temp_head2->n;
+			flipped_values[list_lgt - 1] = temp_head2->n;
 			list_lgt -= 1;
 			temp_head2 = temp_head2->next;
 		}
 		else
 		{
+			if (is_odd == 1)
+			{
+				temp_head2 = temp_head2->next;
+				is_odd = 0;
+			}
+			made_it = 1;
+			printf("flipped_values: %d, n-> values: %d\n", flipped_values[list_lgt], temp_head2->n);
 			if (temp_head2->n != flipped_values[list_lgt])
+			{
+				free(flipped_values);
 				return (0);
+			}
+			list_lgt++;
 			temp_head2 = temp_head2->next;
 		}
 	}
-
+	free(flipped_values);
 	return 1;
 }

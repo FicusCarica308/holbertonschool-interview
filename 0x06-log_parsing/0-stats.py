dfba9ec2086dce98112e4bc6e39a286d9e02d369
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-01 23:52:59 +0000

    changed dictionary name to metrics and added total_size key

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,31 +5,36 @@
 from sys import stdin
 
 
-def print_metrics(total_file_size, http_code_track):
+def print_metrics(metrics):
     """ Temp comment"""
     # prints metrics every 10 lines
-    print("File size: {}".format(total_file_size))
-    for key, value in http_code_track.items():
-        if (value != 0):
+    for key, value in metrics.items():
+        if (key == 'total_size'):
+            print("File size: {}".format(value))
+        elif (value != 0):
             print("{}: {}".format(key, value))
-
 
 if __name__ == "__main__":
     """ Temp comment"""
     # will be used to determine if ten lines has been processed
-    http_code_track = {"200": 0, "301": 0, "400": 0, "401": 0,
+    metrics = {"total_size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
                        "403": 0, "404": 0, "405": 0, "500": 0}
     line_count = 0
     total_file_size = 0
     # loops through stdin given to program
-    for line in stdin:
-        if (line_count == 10):
-            print_metrics(total_file_size, http_code_track)
-            line_count = 0
-        try:
-            arguments = line.split(' ')
-            http_code_track[arguments[7]] += 1
-            total_file_size += int(arguments[8])
-            line_count += 1
-        except Exception:
-            continue
+    try:
+        for line in stdin:
+            if (line_count == 10):
+                print_metrics(metrics)
+                line_count = 0
+            try:
+                arguments = line.split(' ')
+                metrics[arguments[7]] += 1
+                metrics['total_size'] += int(arguments[8])
+                line_count += 1
+            except:
+                continue
+    except:
+        pass
+    finally:
+        print_metrics(metrics)

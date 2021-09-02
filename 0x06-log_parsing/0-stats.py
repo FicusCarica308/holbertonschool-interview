523de53a27463f5aabe612fa2e01a62038ff7b64
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-02 02:52:06 +0000

    added a hopeful change

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,17 +6,17 @@ from sys import stdin
 def print_metrics(metrics, total_size):
     """ Temp comment"""
     # prints metrics every 10 lines
-    codes = ["200", "301", "400", "401",
-             "403", "404", "405", "500"]
+    codes = [200, 301, 400, 401,
+             403, 404, 405, 500]
     print("File size: {}".format(total_size))
-    for code in codes:
-        if metrics[code] > 0:
-            print("{}: {}".format(code, metrics[code]))
+    for code in range(len(codes)):
+        if metrics[codes[code]] > 0:
+            print("{}: {}".format(codes[code], metrics[codes[code]]))
 
 
 # will be used to determine if ten lines has been processed
-metrics = {"200": 0, "301": 0, "400": 0, "401": 0,
-           "403": 0, "404": 0, "405": 0, "500": 0}
+metrics = {200: 0, 301: 0, 400: 0, 401: 0,
+           403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 total_size = 0
 # loops through stdin given to program
@@ -25,7 +25,8 @@ try:
         try:
             line_count += 1
             arguments = line.split()
-            metrics[arguments[-2]] += 1
+            http_code = int(arguments[-2])
+            metrics[http_code] += 1
             total_size += int(arguments[-1])
             if line_count == 10:
                 print_metrics(metrics, total_size)

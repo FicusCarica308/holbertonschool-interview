e8096d9a6a64f4e55bcdf4781ccfacb5d61c504c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-02 01:24:05 +0000

    removed main check

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -14,27 +14,24 @@ def print_metrics(metrics):
             print("{}: {}".format(code, metrics[code]))
 
 
-if __name__ == "__main__":
-    """ Temp comment"""
-    # will be used to determine if ten lines has been processed
-    metrics = {"total_size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
-               "403": 0, "404": 0, "405": 0, "500": 0}
-    line_count = 0
-    # loops through stdin given to program
-    try:
-        for line in stdin:
-            try:
-                arguments = line.split()
-                check = int(arguments[-2])
-                metrics[arguments[-2]] += 1
-                metrics['total_size'] += int(arguments[-1])
-                line_count += 1
-            except Exception:
-                continue
-            if (line_count == 10):
-                print_metrics(metrics)
-                line_count = 0
-    except Exception:
-        pass
-    finally:
-        print_metrics(metrics)
+# will be used to determine if ten lines has been processed
+metrics = {"total_size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
+           "403": 0, "404": 0, "405": 0, "500": 0}
+line_count = 0
+# loops through stdin given to program
+try:
+    for line in stdin:
+        try:
+            arguments = line.split()
+            metrics[arguments[-2]] += 1
+            metrics['total_size'] += int(arguments[-1])
+            line_count += 1
+        except Exception:
+            continue
+        if (line_count == 10):
+            print_metrics(metrics)
+            line_count = 0
+except Exception:
+    pass
+finally:
+    print_metrics(metrics)

d5623ca2569ef70f6ad4e4b6c41b1eb494165f54
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-02 02:03:20 +0000

    added function back

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,22 +3,20 @@
 from sys import stdin
 
 
-# def print_metrics(metrics, total_size):
-#     """ Temp comment"""
-#     # prints metrics every 10 lines
-#     codes = ["200", "301", "400", "401",
-#              "403", "404", "405", "500"]
-#     print("File size: {}".format(total_size))
-#     for code in codes:
-#         if (metrics[code] > 0):
-#             print("{}: {}".format(code, metrics[code]))
+def print_metrics(metrics, total_size):
+    """ Temp comment"""
+    # prints metrics every 10 lines
+    codes = ["200", "301", "400", "401",
+             "403", "404", "405", "500"]
+    print("File size: {}".format(total_size))
+    for code in codes:
+        if (metrics[code] > 0):
+            print("{}: {}".format(code, metrics[code]))
 
 
 # will be used to determine if ten lines has been processed
 metrics = {"200": 0, "301": 0, "400": 0, "401": 0,
            "403": 0, "404": 0, "405": 0, "500": 0}
-codes = ["200", "301", "400", "401",
-         "403", "404", "405", "500"]
 line_count = 0
 total_size = 0
 # loops through stdin given to program
@@ -32,15 +30,9 @@ try:
         except Exception:
             continue
         if (line_count == 10):
-            print("File size: {}".format(total_size))
-            for code in codes:
-                if (metrics[code] > 0):
-                    print("{}: {}".format(code, metrics[code]))
+            print_metrics(metrics, total_size)
             line_count = 0
 except Exception:
     pass
 finally:
-    print("File size: {}".format(total_size))
-    for code in codes:
-        if (metrics[code] > 0):
-            print("{}: {}".format(code, metrics[code]))
+    print_metrics(metrics, total_size)

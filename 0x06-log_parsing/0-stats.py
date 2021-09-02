8b6aba00d43334b927393128611912f678e6d80e
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-02 01:17:53 +0000

    empty commit

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,15 +3,16 @@
 from sys import stdin
 
 
-# def print_metrics(metrics):
-#     """ Temp comment"""
-#     # prints metrics every 10 lines
-#     codes = ["200", "301", "400", "401",
-#              "403", "404", "405", "500"]
-#     print("File size: {}".format(metrics['total_size']))
-#     for code in codes:
-#         if (metrics[code] > 0):
-#             print("{}: {}".format(code, metrics[code]))
+def print_metrics(metrics):
+    """ Temp comment"""
+    # prints metrics every 10 lines
+    codes = ["200", "301", "400", "401",
+             "403", "404", "405", "500"]
+    print("File size: {}".format(metrics['total_size']))
+    for code in codes:
+        print(code)
+        if (metrics[code] > 0):
+            print("{}: {}".format(code, metrics[code]))
 
 
 if __name__ == "__main__":
@@ -19,29 +20,22 @@ if __name__ == "__main__":
     # will be used to determine if ten lines has been processed
     metrics = {"total_size": 0, "200": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "500": 0}
-    codes = ["200", "301", "400", "401",
-            "403", "404", "405", "500"]
     line_count = 0
     # loops through stdin given to program
     try:
         for line in stdin:
             try:
                 arguments = line.split()
+                check = int(arguments[-2])
                 metrics[arguments[-2]] += 1
                 metrics['total_size'] += int(arguments[-1])
                 line_count += 1
             except Exception:
                 continue
             if (line_count == 10):
-                print("File size: {}".format(metrics['total_size']))
-                for code in codes:
-                    if (metrics[code] > 0):
-                        print("{}: {}".format(code, metrics[code]))
+                print_metrics(metrics)
                 line_count = 0
     except Exception:
         pass
     finally:
-        print("File size: {}".format(metrics['total_size']))
-        for code in codes:
-            if (metrics[code] > 0):
-                print("{}: {}".format(code, metrics[code]))
+        print_metrics(metrics)

210fa18486a668bf5e84fb636718946cde688083
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-02 02:20:10 +0000

    added a hopeful fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -6,17 +6,17 @@ from sys import stdin
 def print_metrics(metrics, total_size):
     """ Temp comment"""
     # prints metrics every 10 lines
-    codes = ["200", "301", "400", "401",
-             "403", "404", "405", "500"]
+    codes = [200, 301, 400, 401,
+             403, 404, 405, 500]
     print("File size: {}".format(total_size))
     for code in codes:
-        if (metrics[code] > 0):
-            print("{}: {}".format(code, metrics[code]))
+        if (metrics[int(code)] > 0):
+            print("{}: {}".format(int(code), metrics[int(code)]))
 
 
 # will be used to determine if ten lines has been processed
-metrics = {"200": 0, "301": 0, "400": 0, "401": 0,
-           "403": 0, "404": 0, "405": 0, "500": 0}
+metrics = {200: 0, 301: 0, 400: 0, 401: 0,
+           403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 total_size = 0
 # loops through stdin given to program
@@ -24,7 +24,8 @@ try:
     for line in stdin:
         try:
             arguments = line.split()
-            metrics[arguments[-2]] += 1
+            status_code = int(arguments[-2])
+            metrics[status_code] += 1
             total_size += int(arguments[-1])
             line_count += 1
         except Exception:

dea36a67f9620b65ed52d1abd0362d27eaca41c4
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-07 23:29:47 +0000

    added betty description fix

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * check-cycle - a function in C that checks if a
- *  singly linked list has a cycle in it
- * @list: head of list to be checked
- * Return: returns 0 if no cycle is found, 1 if cycle is found
- */
+* check_cycle - a function in C that checks if a
+* singly linked list has a cycle in it
+* @list: head of list to be checked
+* Return: returns 0 if no cycle is found, 1 if cycle is found
+*/
 int check_cycle(listint_t *list)
 {
 	listint_t *norm_node = list;

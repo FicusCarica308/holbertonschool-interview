3f62ad65e86f84eafd1e96364ecdae2e79ce14bf
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-15 00:37:20 +0000

    added non recursive solution to palindrome problem

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,13 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+/*Libarys*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/* == prototypes == */
+
+/* Finds palindrome of an integer */
+int is_palindrome(unsigned long n);
+
+#endif

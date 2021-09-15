8d72c17a7f573e4ba22ae4532e07af0e480d771c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-15 04:57:08 +0000

    added simpler version of code

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,21 +1,4 @@
 #include "palindrome.h"
-
-/**
- * Pow - helper function that calculate the power of a number
- * @num: the base number
- * @expo: the number's exponent
- * Return: the calculated number of y^x
- */
-int Pow(int num, int expo)
-{
-	int iter = 0;
-	int out = num;
-
-	for (iter = 1; iter < expo; iter++)
-		out = out * num;
-	return (out);
-}
-
 /**
  * is_palindrome - checks if a given unsinged int is a palindrome
  * @n: unsinged integer to be checked
@@ -23,35 +6,16 @@ int Pow(int num, int expo)
  */
 int is_palindrome(unsigned long n)
 {
-	int iter = 0, int_count = 0, div = 0, mod = 0, power = 0;
-	int iter2 = 0;
-	int num_hold = n;
+	unsigned long int hold_n = n;
+	unsigned long int reversed = 0;
 
-	if (n < 10)
+  	while (n != 0)
+  	{
+    		reversed = reversed * 10;
+    		reversed = reversed + n % 10;
+    		n = n / 10;
+  	}
+	if (hold_n == reversed)
 		return (1);
-	/*returns not a palidrome if number is negative*/
-	/* negative numbers passed to unsigned int are greater than UINT_MAX */
-	if (n > 4294967295)
-		return (0);
-	for (int_count = 0; num_hold != 0; int_count++)
-		num_hold = num_hold / 10;
-	num_hold = n;
-	for (iter = int_count - 1; iter >= 0 && num_hold > 0; iter--)
-	{
-		if (iter == int_count - 1)
-			power = Pow(10, iter);
-		else
-			power = Pow(10, iter - iter2);
-		iter2++;
-		mod = num_hold % 10;
-		if (num_hold > 9)
-			div = num_hold / power;
-		else
-			div = num_hold / 1;
-		if (div != mod)
-			return (0);
-		num_hold = num_hold - power * div;
-		num_hold = num_hold / 10;
-	}
-	return (1);
+	return (0);
 }

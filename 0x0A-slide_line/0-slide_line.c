005b1491d93aecdd7578ff0566119b247d8c88ad
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-09-29 04:36:11 +0000

    removed uneccessary curly braces for overarching if statments

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -22,7 +22,6 @@ int slide_line(int *line, size_t size, int direction)
 	int i = 0, k = 0, j = 0, index_max = size - 1;
 
 	if (direction == SLIDE_RIGHT)
-	{
 		for (i = index_max; i >= 0; i--)
 		{
 			for (k = i - 1; k >= 0; k--)
@@ -38,10 +37,7 @@ int slide_line(int *line, size_t size, int direction)
 					line[j] = 0;
 				}
 		}
-		return (1);
-	}
-	if (direction == SLIDE_LEFT)
-	{
+	else if (direction == SLIDE_LEFT)
 		for (i = 0; i <= index_max; i++)
 		{
 			for (k = i + 1; k <= index_max; k++)
@@ -57,7 +53,7 @@ int slide_line(int *line, size_t size, int direction)
 					line[j] = 0;
 				}
 		}
-		return (1);
-	}
-	return (0);
+	else
+		return (0);
+	return (1);
 }

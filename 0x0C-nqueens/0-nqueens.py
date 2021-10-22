09c99e8c8fbdeef39b23b44b572e8d34d6f44e2c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-10-22 18:44:36 +0000

    added code to find lower left and right diagnals and lower column

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,6 +3,41 @@
 """
 from sys import argv
 
+def populate_board(size):
+    board = []
+    board_row = []
+    for i in range(0, size):
+        board_row.append(i + 1)
+    for j in range(0, size):
+        board.append(board_row)
+    return board
+
+def get_attacks(board, row, column):
+    vert = []
+    right_diag = []
+    left_diag = []
+
+    # gets vertical attack pattern for position
+    for i in range(len(board)):
+        vert.append(board[i][column])
+    total = 0
+    for i in range(1, len(board)):
+        if (column + i < len(board)):
+            right_diag.append(board[i][column + i])
+    for i in range(len(board), 0, -1):
+        if (column - i > -1):
+            left_diag.append(board[i][column - i])
+    return vert, right_diag, left_diag
+        
+    # gets diagnal attack pattern for position
+def validate_position():
+    pass
+
+
+def place_queen(size, board):
+    pass
+    
+
 
 if __name__ == "__main__":
     """Checks"""
@@ -20,3 +55,6 @@ if __name__ == "__main__":
     
     """Solution"""
     
+    board = populate_board(N)
+    print(get_attacks(board, 0, 0))
+    print(board)

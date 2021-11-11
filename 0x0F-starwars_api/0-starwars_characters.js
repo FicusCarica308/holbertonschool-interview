b1b295593aa49195aab127be6b1b9cf730d25e53
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-11 23:39:51 +0000

    added promise

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,13 +5,14 @@ const argv= process.argv.slice(2);
 
 star_wars_movie_id = argv[0]
 
-request(`https://swapi-api.hbtn.io/api/films/${star_wars_movie_id}`, function (error, response, body) {
-    const characters = JSON.parse(body)['characters'];
-    for (let character of characters) {
-            console.log(character)
-            request(character, function (error, response, body) {
-                let name = JSON.parse(body)['name']
-                console.log(name)
+request(`https://swapi-api.hbtn.io/api/films/${star_wars_movie_id}`, async function (error, response, body) {
+    const character_urls = JSON.parse(body)['characters'];
+    for (let url of character_urls) {
+        await new Promise(function (resolve, reject) {
+            request(url, function (error, response, body) {
+                console.log(JSON.parse(body)['name'])
+                resolve()
             });
+        });
     }
 });

2761b8b018945fe2b084065f5ac26b3d5c519c7f
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-11 23:45:54 +0000

    added semistandard code fixes/changes

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,18 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const argv= process.argv.slice(2);
+const argv = process.argv.slice(2);
 
-star_wars_movie_id = argv[0]
+const movieId = argv[0];
 
-request(`https://swapi-api.hbtn.io/api/films/${star_wars_movie_id}`, async function (error, response, body) {
-    const character_urls = JSON.parse(body)['characters'];
-    for (let url of character_urls) {
-        await new Promise(function (resolve, reject) {
-            request(url, function (error, response, body) {
-                console.log(JSON.parse(body)['name'])
-                resolve()
-            });
-        });
-    }
+request(`https://swapi-api.hbtn.io/api/films/${movieId}`, async function (error, response, body) {
+  if (error) {
+    return;
+  }
+  const characterURLS = JSON.parse(body).characters;
+  for (const url of characterURLS) {
+    await new Promise(function (resolve, reject) {
+      request(url, function (error, response, body) {
+        if (error) {
+          return;
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });

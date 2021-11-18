50be04aceb38481e2e09c16058399ab3e0b3e2c3
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-11-18 00:49:57 +0000

    added solution to problem

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def rain(walls):
+    spaces_between = 0
+    prev_wall = 0
+    total_water = 0
+    
+    for wall in walls:
+        if (wall != 0 and prev_wall == 0):
+            prev_wall = wall
+            continue
+        if (wall != 0):
+            if (wall < prev_wall):
+                lost_space = prev_wall - wall
+                total_water += (prev_wall - lost_space) * spaces_between
+                spaces_between = 0
+                prev_wall = wall
+            if (wall > prev_wall):
+                lost_space = (prev_wall - wall) * -1
+                total_water += (wall - lost_space) * spaces_between
+                spaces_between = 0
+                prev_wall = wall
+        else:
+            if (prev_wall != 0):
+                spaces_between += 1
+    return total_water

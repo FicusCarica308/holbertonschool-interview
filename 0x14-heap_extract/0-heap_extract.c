932bdd7d161d42e73ef948d9f33135bb6bfac551
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-16 06:37:46 +0000

    added rewrite of code plus old code

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,4 +1,28 @@
 #include "binary_trees.h"
+
+int tree_size(heap_t *root)
+{
+	if (root == NULL)
+		return (0);
+	else
+		return (tree_size(root->left) + 1 + tree_size(root->right));
+}
+
+void path_to_last_node(int tree_size, long int return_values[])
+{
+	long int binary_path = 0, iteration = 1, remainder;
+
+	while (tree_size != 0)
+	{
+		remainder = tree_size % 2;
+		binary_path = binary_path + remainder * iteration;
+		iteration = iteration * 10;
+		tree_size = tree_size / 2;
+	}
+	return_values[0] = binary_path;
+	return_values[1] = iteration / 100;
+}
+
 /**
 * heap_extract - a function that extracts the root node of a Max Binary Heap
 * @root: is a double pointer to the root node of the heap
@@ -6,44 +30,46 @@
 */
 int heap_extract(heap_t **root)
 {
-	heap_t *tmp_head = NULL;
-	int hold_n, right_or_left;
+	long int node_path_info[2], node_path, node_start_point;
+	heap_t *temp_head = *root;
+	(void) temp_head;
 
 	if (root == NULL)
 		return (0);
-	tmp_head = *root;
-	if (tmp_head->right == NULL && tmp_head->left == NULL)
+	
+	/* Init all variables*/
+	path_to_last_node(tree_size(*root), node_path_info);
+	node_path = node_path_info[0];
+	node_start_point = node_path_info[1];
+
+	printf("path = %ld ------ starting_point = %ld\n", node_path, node_start_point);
+
+	while (node_start_point > 1)
 	{
-		hold_n = tmp_head->n;
-		*root = NULL;
-		free(tmp_head);
-		return (hold_n);
-	}
-	while (1 != 0)
-	{
-		hold_n = tmp_head->n;
-		if (tmp_head->right == NULL || tmp_head->left->n >= tmp_head->right->n)
+		node_path = node_path / 10;
+		if ((node_path / node_start_point) == 0)
 		{
-			tmp_head->n = tmp_head->left->n;
-			tmp_head->left->n = hold_n;
-			tmp_head = tmp_head->left;
-			right_or_left = 0;
-		} else if (tmp_head->left == NULL || tmp_head->right->n >= tmp_head->left->n)
-		{
-			tmp_head->n = tmp_head->right->n;
-			tmp_head->right->n = hold_n;
-			tmp_head = tmp_head->right;
-			right_or_left = 1;
+			temp_head = temp_head->left;
+			printf("ran1");
 		}
-		if (tmp_head->right == NULL && tmp_head->left == NULL)
+		else if ((node_path / node_start_point) == 1)
 		{
-			if (right_or_left == 0)
-				tmp_head->parent->left = NULL;
-			else if (right_or_left == 1)
-				tmp_head->parent->right = NULL;
-			free(tmp_head);
-			return (hold_n);
+			temp_head = temp_head->right;
+			printf("ran2");
 		}
+		node_start_point = node_start_point / 10;
 	}
+
+	printf("Found node = %d", temp_head->n);
+
+	/* 
+	Find node to delete
+	save the value of that node
+	Free the node
+	
+	Move the root down to the bottom
+	once the root has reached the bottom swap the values with the saved value from delete node
+	*/
+
 	return (0);
 }

46ba29200c9f25745e3cbd35cf42c5dd85d38a29
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-16 23:47:26 +0000

    reverted all betty style changes

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,21 +1,71 @@
 #include "binary_trees.h"
 
+#include "binary_trees.h"
 /**
- * free_last_node - frees the last node of a binary tree and returns its ->n
- *@root: the root node of the binary tree
- *Return: returns the ->n value of the deleted node
-*/
-int free_last_node(heap_t *root)
+ * left_and_right - loops through tree recursively left and right
+ * @tree: given tree
+ * Return: returns total height of tree
+ */
+size_t left_and_right(const binary_tree_t *tree)
 {
-	int size = tree_size(root);
-	heap_t *temp_head = root;
-	int height = binary_tree_height(root);
+	int left_height = 0;
+	int right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+	left_height = left_and_right(tree->left) + 1;
+	right_height = left_and_right(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
+/**
+ * binary_tree_height - finds height from node
+ * @tree: given tree
+ * Return: height;
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+		return (left_and_right(tree) - 1);
+}
+
+int tree_size(heap_t *root)
+{
+	if (root == NULL)
+		return (0);
+	else
+		return (tree_size(root->left) + 1 + tree_size(root->right));
+}
+
+/**
+ *print_binary - prints a decimal number as binary
+ *@n: the decimal number given
+ */
+void print_binary(unsigned long int n, int *binary, int current_index)
+{
+	unsigned long int temp = n;
+	if (temp == 1)
+		return;
+	if (temp > 1)
+		print_binary(temp >> 1, binary, current_index - 1);
+	binary[current_index] = (temp & 1);
+}
+
+int heap_extract(heap_t **root)
+{
+	int size = tree_size(*root);
+	heap_t *temp_head = *root;
+	int height = binary_tree_height(*root);
 	int *binary = malloc(sizeof(int) * height);
 	int i;
-	int hold_n1;
+	int hold_n1, hold_n2;
 
-	/* Finds and removes the last item in the binary tree */
-	get_last_node_path(size, binary, height - 1);
+		/* Finds and removes the last item in the binary tree */
+	print_binary(size, binary, height - 1);
 
 	printf("\n");
 	for (i = 0; i < height; i++)
@@ -33,44 +83,31 @@ int free_last_node(heap_t *root)
 	hold_n1 = temp_head->n;
 	free(temp_head);
 	free(binary);
-	return (hold_n1);
-}
 
-/**
-* heap_extract - a function that extracts the root node of a Max Binary Heap
-* @root: is a double pointer to the root node of the heap
-* Return: returns 0 on failure or the ->n value of the node extracted
-*/
-int heap_extract(heap_t **root)
-{
-	heap_t *tmp_head = *root;
-	int hold_n1, hold_n2;
+	/* ======= */
 
-	hold_n1 = free_last_node(*root);
-
-	tmp_head = *root;
+	temp_head = *root;
 
 	while (1 != 0)
 	{
-		hold_n2 = tmp_head->n;
-		if (tmp_head->right == NULL || tmp_head->left->n >= tmp_head->right->n)
+		hold_n2 = temp_head->n;
+		if (temp_head->right == NULL || temp_head->left->n >= temp_head->right->n)
 		{
-			tmp_head->n = tmp_head->left->n;
-			tmp_head->left->n = hold_n2;
-			tmp_head = tmp_head->left;
+			temp_head->n = temp_head->left->n;
+			temp_head->left->n = hold_n2;
+			temp_head = temp_head->left;
+		} 
+		else if (temp_head->left == NULL || temp_head->right->n >= temp_head->left->n)
+		{
+			temp_head->n = temp_head->right->n;
+			temp_head->right->n = hold_n2;
+			temp_head = temp_head->right;
 		}
-		else if (tmp_head->left == NULL || tmp_head->right->n >= tmp_head->left->n)
+		if (temp_head->right == NULL && temp_head->left == NULL)
 		{
-			tmp_head->n = tmp_head->right->n;
-			tmp_head->right->n = hold_n2;
-			tmp_head = tmp_head->right;
-		}
-		if (tmp_head->right == NULL && tmp_head->left == NULL)
-		{
-			tmp_head->n = hold_n1;
+			temp_head->n = hold_n1;
 			return (hold_n2);
 		}
 	}
-
 	return (0);
 }

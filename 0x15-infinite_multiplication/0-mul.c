b2ca474b46abc46e2a4895c77e9d6813aa47ee83
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-23 22:47:40 +0000

    removed prev changes

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-void error(void)
+void error()
 {
 	_putchar('E');
 	_putchar('r');
@@ -33,7 +33,7 @@ int *stoi(char *string)
 	if (large_num == NULL)
 		error();
 
-	for (i = 0;  string[i] != '\0'; i++)
+	for(i = 0;  string[i] != '\0'; i++)
 	{
 		if ((int)string[i] > 58 || (int)string[i] < 48)
 			error();
@@ -47,40 +47,22 @@ int main(int argc, char **argv)
 	int *int_1 = NULL;
 	int *int_2 = NULL;
 	int *new_num = NULL;
-	int hold_new_value;
 	int i;
-	int back_i = 0;
-	int back_j = 0;
-	int j;
-	int hold_carry;
-	int k;
+	(void) new_num;
 
 	if (argc != 3)
 		error();
 	int_1 = stoi(argv[1]);
 	int_2 = stoi(argv[2]);
-	new_num = malloc(sizeof(int *) * (strlgth(argv[1]) + strlgth(argv[2])));
+	new_num = malloc(sizeof(int) * strlgth(argv[1]) + strlgth(argv[2]));
 	if (new_num == NULL)
 		error();
-	
-	for (i = strlgth(argv[1]) - 1; i >= 0; i--, back_i++)
-	{
-		hold_carry = 0;
-		back_j = 0;
-		for (j = strlgth(argv[2]) - 1; j >= 0; j--, back_j++)
-		{
-			hold_new_value = int_1[i] * int_2[j] + new_num[back_i + back_j] + hold_carry;
-			hold_carry = hold_new_value / 10;
-			new_num[back_i + back_j] = hold_new_value % 10;
-			back_j++;
-		}
-		if (hold_carry > 0)
-			new_num[back_i + back_j] += hold_carry;
-		back_i++;
-	}
 
-	for (k = strlgth(argv[1]) + strlgth(argv[2]) - 1; k >= 0; k--)
-		_putchar(new_num[k] + '0');
+	for (i = 0; i <= 1; i++)
+	{
+		printf("%d", int_1[i]);
+	}
+	printf("\n");
 	free(int_1);
 	free(int_2);
 	free(new_num);

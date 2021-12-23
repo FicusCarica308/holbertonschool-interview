ef25596f08def8a82dbd6d70318b57656f1ace7c
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2021-12-23 22:58:33 +0000

    added betty linter fixes

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
-void error()
+
+/**
+* error - prints error and exists program with status 98
+*/
+void error(void)
 {
 	_putchar('E');
 	_putchar('r');
@@ -12,6 +15,11 @@ void error()
 	exit(98);
 }
 
+/**
+ * strlgth - finds the length of a string
+ * @string: The string we are measuring
+ * Return: returns the computed length
+*/
 int strlgth(char *string)
 {
 	int strlgth = 0;
@@ -22,6 +30,11 @@ int strlgth(char *string)
 	return (strlgth);
 }
 
+/**
+ * stoi - converts a given string into a integer array
+ * @string: the string we are converting
+ * Return: returns the array representation of the string
+ */
 int *stoi(char *string)
 {
 	int i = 0;
@@ -33,7 +46,7 @@ int *stoi(char *string)
 	if (large_num == NULL)
 		error();
 
-	for(i = 0;  string[i] != '\0'; i++)
+	for (i = 0;  string[i] != '\0'; i++)
 	{
 		if ((int)string[i] > 58 || (int)string[i] < 48)
 			error();
@@ -42,27 +55,45 @@ int *stoi(char *string)
 	return (large_num);
 }
 
+/**
+ * main - Entry point
+ * @argv: command line arguments
+ * @argc: command line argument count
+ * Return: 0 on success, 98 on failure
+ */
 int main(int argc, char **argv)
 {
-	int *int_1 = NULL;
-	int *int_2 = NULL;
-	int *new_num = NULL;
-	int i;
-	(void) new_num;
+	int *int_1 = NULL, *int_2 = NULL, *new_num = NULL;
+	int hold_new, hold_carry;
+	int i, j, k;
+	int back_i = 0, back_j;
 
 	if (argc != 3)
 		error();
 	int_1 = stoi(argv[1]);
 	int_2 = stoi(argv[2]);
-	new_num = malloc(sizeof(int) * strlgth(argv[1]) + strlgth(argv[2]));
+	new_num = malloc(sizeof(int *) * (strlgth(argv[1]) + strlgth(argv[2])));
 	if (new_num == NULL)
 		error();
 
-	for (i = 0; i <= 1; i++)
+	for (i = strlgth(argv[1]) - 1; i >= 0; i--, back_i++)
 	{
-		printf("%d", int_1[i]);
+		hold_carry = 0;
+		back_j = 0;
+		for (j = strlgth(argv[2]) - 1; j >= 0; j--, back_j++)
+		{
+			hold_new = int_1[i] * int_2[j] + new_num[back_i + back_j] + hold_carry;
+			hold_carry = hold_new / 10;
+			new_num[back_i + back_j] = hold_new % 10;
+			back_j++;
+		}
+		if (hold_carry > 0)
+			new_num[back_i + back_j] += hold_carry;
+		back_i++;
 	}
-	printf("\n");
+
+	for (k = strlgth(argv[1]) + strlgth(argv[2]) - 1; k >= 0; k--)
+		_putchar(new_num[k] + '0');
 	free(int_1);
 	free(int_2);
 	free(new_num);

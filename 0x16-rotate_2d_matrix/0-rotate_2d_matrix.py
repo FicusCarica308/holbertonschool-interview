7fe11adc1d17130624067dd357004bb8c54f81ba
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-01-06 19:30:56 +0000

    added solution to rotate matrix problem

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def rotate_2d_matrix(matrix):
+    """ Rotates a NxN matrix 90 degrees """
+    size = len(matrix[0])
+    transpose = list(map(list, zip(*matrix)))
+    for row in range(size):
+        for column in range(size - 1, -1, -1):
+            matrix[row][abs(column - (size - 1))] = transpose[row][column]

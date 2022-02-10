38e124b4c4d5d0addd223123585fa37af1df9aed
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-10 22:53:08 +0000

    added all starter files for inteview question

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/**
+ * add_node_end - adds a new node to the end of a double circular linked list.
+ * @list: The list to modify
+ * @str: The string to copy into the new node
+ * Return: Returns new node
+*/
+List *add_node_end(List **list, char *str)
+{
+
+}
+
+/**
+ * add_node_begin - Adds a new node to the beginning of a double circular
+ *					linked list
+ * @list: The list to modify
+ * @str: The string to copy into the new node
+ * Return: Returns new node
+*/
+List *add_node_begin(List **list, char *str);

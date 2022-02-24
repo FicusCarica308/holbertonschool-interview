709b838e1ae3199297013d0041ae7252a87329eb
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-02-24 22:39:54 +0000

    pycodestyle error fix

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """
-Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
+Create a function def island_perimeter(grid): that returns the perimeter
+of the island described in grid:
 
     grid is a list of list of integers:
         0 represents water
@@ -11,8 +11,10 @@ Create a function def island_perimeter(grid): that returns the perimeter of the 
         grid is rectangular, with its width and height not exceeding 100
     The grid is completely surrounded by water
     There is only one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water surrounding the island).
+    The island doesn’t have “lakes” (water inside that isn’t connected
+    to the water surrounding the island).
 """
+
 
 def island_perimeter(grid):
     """ Returns the perimeter of the island described in grid """

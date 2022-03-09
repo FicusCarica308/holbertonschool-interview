a1d5f63d0b5d9e036e0df646826200a18f604e35
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-09 12:22:08 +0000

    added betty linter fix's

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 
 /**
- * 
- * 
+ * wildcmp - a function that compares two strings
+ * @s1: A normal point to a string
+ * @s2: A pointer to a string that can contain the special character *.
+ * Return:  1 if the strings can be considered identical, otherwise 0
 */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	
+
 	if (*s1 == '\0' && *s2 != '\0' && *s2 != '*')
 		return (0);
-	
+
 	if (*s1 != '\0' && *s2 == '\0')
 		return (0);
-	
+
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
@@ -25,12 +27,11 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1 + 1, s2));
 		else
 			return (wildcmp(s1, s2 + 1));
-	} else {
+	} else
+	{
 		if (*s1 != *s2 && *(s2 + 1) == '*')
 			return (0);
 		return (wildcmp(s1 + 1, s2));
 	}
-	
-	
 	return (0);
 }

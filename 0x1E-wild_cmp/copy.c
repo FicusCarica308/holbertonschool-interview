5c0ed4aede9fc10fcd0e2514b1c36d318e695319
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-09 11:30:37 +0000

    added edited copy of task 0

---
+++ 0x1E-wild_cmp/copy.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * 
+ * 
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	
+	if (*s1 == '\0' && *s2 != '\0' && *s2 != '*')
+		return (0);
+	
+	if (*s1 != '\0' && *s2 == '\0')
+		return (0);
+	
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return  (wildcmp(s1, s2 + 1));
+		if (*s1 != *(s2 + 1))
+			return (wildcmp(s1 + 1, s2));
+		else
+			return (wildcmp(s1, s2 + 1));
+	} else {
+		return (wildcmp(s1 + 1, s2));
+	}
+	
+	
+	return (0);
+}

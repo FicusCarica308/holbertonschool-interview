f61bc05ba40cd27d66555a48d30b48ca1b5369d6
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-24 22:15:39 +0000

    added header and starter file

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,8 @@
+#include "regex.h"
+
+int regex_match(char *str, char *pattern)
+{
+    if (str == pattern)
+        return (1);
+    return (1);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,7 @@
+#ifndef _REGEX_H_
+#define _REGEX_H_
+
+/* Prototypes */
+int regex_match(char const *str, char const *pattern);
+
+#endif-

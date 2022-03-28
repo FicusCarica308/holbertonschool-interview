1ab263945d2fa22d5c5f357527d24d96987943df
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-03-28 22:17:29 +0000

    added prototype and documentation for find_substring() function

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,26 @@
+#include "substring.h"
+
+/**
+ * find_substring - finds all the possible substrings containing
+ *                  a list of words, within a given string.
+ * @s: is the string to scan
+ * @words: is the array of words all substrings must be a
+ *         concatenation arrangement of
+ * @nb_words: is the number of elements in the array words
+ * @n: holds the address at which to store the number of
+ *     elements in the returned array
+ * Return: An allocated array, storing each index in 's',
+ *         at which the substring was found... NULL if no solution
+ * Notes:
+ *  > A valid substring of s is the concatenation of
+ *    each word in words exactly once and without any intervening characters
+ *  > All words in the array words are the same length
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+    (void)s;
+    (void)words;
+    (void)nb_words;
+    (void)n;
+	return (NULL);
+}

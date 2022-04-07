f90b732839ccbd59274e61fe4cbe6383bf0978ff
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-07 05:21:15 +0000

    added solution to interview question 'primegame'

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""_summary_
+    maria alwasy goes first
+"""
+
+
+def isPrime(n):
+    for i in range(2, int(n / 2) + 1):
+        if (n % i) == 0:
+            break
+    else:
+        return (n)  # is prime
+
+
+def isWinner(x, nums):
+    """ : """
+    maria_wins = 0
+    ben_wins = 0
+    player_win = 'false'
+
+    for round in range(0, x):
+        round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
+        if (len(round_nums) % 2 == 0):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+    if (maria_wins == ben_wins):
+        return (None)
+    elif (maria_wins > ben_wins):
+        return ('Maria')
+    return ('Ben')

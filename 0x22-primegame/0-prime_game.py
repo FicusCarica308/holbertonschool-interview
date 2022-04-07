90c6c107828dd9378586154d2ed2281295a2caf1
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-07 19:13:26 +0000

    added another edgecase that checks if the number of rounds exceeds the index of the passed array 'nums'

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -16,6 +16,9 @@ def isWinner(x, nums):
     """ : """
     maria_wins = 0
     ben_wins = 0
+    
+    if (x > len(nums)):
+        return (None)
 
     for round in range(0, x):
         round_nums = list(filter(isPrime, range(1, nums[round] + 1)))

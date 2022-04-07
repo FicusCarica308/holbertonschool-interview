22561b4d22086001e61d5b97f33bc2f60f27df69
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-07 19:15:50 +0000

    moved edgecase logic to inside for loop in isWinner() function

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -16,16 +16,14 @@ def isWinner(x, nums):
     """ : """
     maria_wins = 0
     ben_wins = 0
-    
-    if (x > len(nums)):
-        return (None)
 
     for round in range(0, x):
-        round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
-        if (len(round_nums) % 2 == 0):
-            maria_wins += 1
-        else:
-            ben_wins += 1
+        if (round < len(nums)):
+            round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
+            if (len(round_nums) % 2 == 0):
+                maria_wins += 1
+            else:
+                ben_wins += 1
 
     if (maria_wins == ben_wins):
         return (None)

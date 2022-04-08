26d125dee556a9b40f190047439e38cdaacd2c88
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 02:13:51 +0000

    didnt include 1 in round_nums array

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -6,7 +6,7 @@
 
 def isPrime(n):
     for i in range(2, int(n / 2) + 1):
-        if (n % i) == 0:
+        if ((n % i) == 0):
             break
     else:
         return (n)  # is prime
@@ -19,9 +19,10 @@ def isWinner(x, nums):
 
     for round in range(0, x):
         if (round < len(nums)):
-            # removes all non-prime numbers from the round number set
-            round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
-            if (len(round_nums) % 2 == 0):
+            # removes all non-prime numbers from the round number set 
+            round_nums = list(filter(isPrime, range(2, nums[round] + 1)))
+            print(round_nums)
+            if (len(round_nums) % 2 != 0):
                 maria_wins += 1
             else:
                 ben_wins += 1

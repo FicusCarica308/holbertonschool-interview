73946a49754635872facb69d3f2e346d811c1bd0
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 02:23:07 +0000

    stopped including 1 in round_nums again

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -20,8 +20,8 @@ def isWinner(x, nums):
     for round in range(0, x):
         if (round < len(nums)):
             # removes all non-prime numbers from the round number set 
-            round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
-            if (len(round_nums) % 2 == 0):
+            round_nums = list(filter(isPrime, range(2, nums[round] + 1)))
+            if (len(round_nums) % 2 != 0):
                 maria_wins += 1
             else:
                 ben_wins += 1

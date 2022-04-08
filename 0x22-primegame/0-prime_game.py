46a5e36668f4bc1f0384a3171eb38b18e2ea8ef2
Manuel Enrique Figueroa <manuelthemuffin@gmail.com>
2022-04-08 02:16:20 +0000

    reverted code back to original version (including 1 in round_nums)

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -20,9 +20,9 @@ def isWinner(x, nums):
     for round in range(0, x):
         if (round < len(nums)):
             # removes all non-prime numbers from the round number set 
-            round_nums = list(filter(isPrime, range(2, nums[round] + 1)))
+            round_nums = list(filter(isPrime, range(1, nums[round] + 1)))
             print(round_nums)
-            if (len(round_nums) % 2 != 0):
+            if (len(round_nums) % 2 == 0):
                 maria_wins += 1
             else:
                 ben_wins += 1
